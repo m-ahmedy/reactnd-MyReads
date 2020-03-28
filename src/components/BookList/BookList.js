019ec1495454c9469ce0774b9e9a15c9587f8c26
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-03-29 00:58:03 +0200

    feat: Book list creates fake books dynamically

--- src/components/BookList/BookList.js
+++ src/components/BookList/BookList.js
@@ -1,8 +1,27 @@
-/*
-<div className="bookshelf-books">
-    <ol className="books-grid">
-        <Book />
-        <Book />
-    </ol>
-</div>
-*/
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const BookList = props => {
+    const { bookList, className } = props;
+
+    const bookItems = bookList.length 
+        ? bookList.map(book => (
+            <p key={book}>
+                A book
+            </p>
+        ))
+        : null;
+    
+    return (
+        <div className={className ? className : null}>
+            { bookItems }
+        </div>
+    )
+}
+
+BookList.propTypes = {
+    bookList: PropTypes.array.isRequired,
+    className: PropTypes.string
+}
+
+export default BookList
--- src/components/Shelf/Shelf.js
+++ src/components/Shelf/Shelf.js
@@ -1,20 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import BookList from '../BookList/BookList'
 
 const Shelf = props => {
+    const { title } = props;
+
+    // Test
+    const bookList = [];
     return (
         <div className="bookshelf">
-            <h2 className="bookshelf-title">{props.title}</h2>
+            <h2 className="bookshelf-title">{title}</h2>
             <div className="bookshelf-books">
-                {/* <BookList /> */}
-                BookList
+                <BookList
+                    className='books-grid'
+                    bookList={bookList}
+                />
             </div>
         </div>
     )
 }
 
 Shelf.propTypes = {
-    title: PropTypes.string.isRequired
+    title: PropTypes.string.isRequired,
+    bookList: PropTypes.array
 };
 
 export default Shelf
