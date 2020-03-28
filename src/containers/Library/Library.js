140dfc8cba33e061a03b271c817cab2a4801a26e
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-03-29 00:15:53 +0200

    feat: Books are fetched and stored in state

--- src/containers/Library/Library.js
+++ src/containers/Library/Library.js
@@ -1,31 +1,47 @@
-/*
-<div className="list-books">
-    <div className="list-books-title">
-        <h1>MyReads</h1>
-    </div>
-    <div className="list-books-content">
-        <div>
-            <Shelf />
-            <Shelf />
-            <Shelf />
-        </div>
-    </div>
-    <Button />
-</div>
-
-*/
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
+import * as BooksAPI from '../../apis/BooksAPI'
 
 export default class Library extends Component {
     static propTypes = {
         prop: PropTypes
     }
 
+    state = {
+        books: []
+    }
+
+    componentDidMount () {
+        BooksAPI.getAll()
+            .then(fetchedBooks => {
+                console.log(fetchedBooks);
+
+                const bookInfo = fetchedBooks.map(book => ({
+                    ...book,
+                    shelf: 'None'
+                }));
+
+                console.log(bookInfo);
+                this.setState(prevState => ({
+                    books: bookInfo
+                }));
+            });
+    }
+
     render() {
         return (
-            <div>
-                Library
+            <div className="list-books">
+                <div className="list-books-title">
+                    <h1>MyReads</h1>
+                </div>
+                <div className="list-books-content">
+                    <div>
+                        {/* <Shelf />
+                        <Shelf />
+                        <Shelf /> */}
+                    </div>
+                </div>
+                {/* <Button /> */}
             </div>
         )
     }
