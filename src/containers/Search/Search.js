824c3075a08e3593a5eb65a1cfc6f36567b02889
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-03-29 02:53:17 +0200

    feat: Created Search page

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,31 +1,27 @@
-/*
-<div className="search-books">
-    <div className="search-books-bar">
-        <Button />
-        <div className="search-books-input-wrapper">
-            
-            <input type="text" placeholder="Search by title or author" />
-
-        </div>
-    </div>
-    <div className="search-books-results">
-        <BookList />
-    </div>
-</div>
-*/
-
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+import Button from '../../components/UI/Button/Button'
+import BookList from '../../components/BookList/BookList'
 
 export default class Search extends Component {
-    static propTypes = {
-        prop: PropTypes
-    }
-
     render() {
         return (
-            <div>
-                Search
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Button 
+                        className='close-search'
+                        linkTo='/'
+                    />
+                    <div className="search-books-input-wrapper">
+
+                        <input type="text" placeholder="Search by title or author" />
+
+                    </div>
+                </div>
+                <div className="search-books-results">
+                    <BookList
+                        bookList={[]}
+                    />
+                </div>
             </div>
         )
     }
