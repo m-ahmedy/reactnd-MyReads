16f4f9221d9e319c5fc5331e95075021d5c237cb
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-03-29 03:12:54 +0200

    feat: Provides search capability

--- src/containers/Search/Search.js
+++ src/containers/Search/Search.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react'
+import * as BooksAPI from '../../apis/BooksAPI'
 import Button from '../../components/UI/Button/Button'
 import BookList from '../../components/BookList/BookList'
 
 export default class Search extends Component {
+    state = {
+        query: '',
+        queryBooks: []
+    }
+
+    queryHandler = (query) => {
+        this.setState(prevState => ({
+            query: query.trim()
+        }));
+    }
+
+    searchWithQuery = (event) => {
+        const { query } = this.state;
+
+        if (event.key === 'Enter') {
+            BooksAPI.getAll()
+                .then(fetchedBooks => {
+                    const queryBooks = fetchedBooks.filter(book => book.title.toLowerCase().includes(query.toLowerCase()));
+
+                    this.setState(prevState => ({
+                        queryBooks: queryBooks
+                    }))
+                });
+        }
+    }
+
     render() {
         return (
             <div className="search-books">
@@ -13,13 +40,22 @@ export default class Search extends Component {
                     />
                     <div className="search-books-input-wrapper">
 
-                        <input type="text" placeholder="Search by title or author" />
+                        <input
+                            type="text"
+                            placeholder="Search by title or author"
+                            value={this.state.query}
+                            onChange={(event) => this.queryHandler(event.target.value)}
+                            onKeyUp={this.searchWithQuery}
+                        />
 
                     </div>
                 </div>
                 <div className="search-books-results">
                     <BookList
-                        bookList={[]}
+                        className='books-grid'
+                        bookList={this.state.queryBooks}
+                        
+                        shelfOptions={this.props.shelves}
                     />
                 </div>
             </div>
