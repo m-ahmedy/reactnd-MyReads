50fcc936871b229d86eec2ab9acbd2eeb51d2ea9
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2020-03-29 02:52:36 +0200

    feat: Close search button created

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Button = props => {
-    const { className, linkTo } = props; 
+    const { className, linkTo, title } = props; 
     return (
         <div className={className}>
-            <Link to={linkTo}>Add a book</Link>
+            <Link
+                to={linkTo}
+            >
+                {title}
+            </Link>
         </div>
     )
 }
